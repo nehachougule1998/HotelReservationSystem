fe46aaa6fa5b1d9ca1f692b96e9b9bbc13362f4b
nehachougule1998 <nchougule734@gmail.com>
2022-04-16 11:50:18 +0530

    Added

--- HotelReservationSystem/src/main/java/Workshop/HotelReservationSystem/HotelReservationSystem.java
+++ HotelReservationSystem/src/main/java/Workshop/HotelReservationSystem/HotelReservationSystem.java
@@ -1,10 +1,9 @@
 package Workshop.HotelReservationSystem;
 
 public class HotelReservationSystem {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation System Program");
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+        HotelSetup hotelSetup=new HotelSetup();
+        hotelSetup.addHotel();
+    }
 }
--- HotelReservationSystem/src/main/java/Workshop/HotelReservationSystem/HotelSetup.java
+++ HotelReservationSystem/src/main/java/Workshop/HotelReservationSystem/HotelSetup.java
@@ -1,26 +1,19 @@
 package Workshop.HotelReservationSystem;
 
+
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class HotelSetup {
-	  ArrayList<Hotel1> hotelList = new ArrayList<>();
-	
-	void addHotel(){
-        Scanner scan = new Scanner(System.in);
-        Hotel1 newHotel = new Hotel1();
-        System.out.println("Enter Name of the hotel: ");
-        String name = scan.next();
-        newHotel.setName(name);
-        System.out.println("Enter rates for regular customer on Weekdays: ");
-        Integer wdRegular = scan.nextInt();
-        newHotel.setWD_REGULAR(wdRegular);
-        System.out.println("Enter rates for regular customer on Weekends: ");
-        Integer weRegular = scan.nextInt();
-        newHotel.setWE_REGULAR(weRegular);
-        hotelList.add(newHotel);
-        
-        }
+    ArrayList<Hotel> myHotelList = new ArrayList();                                                //Array
+
+    public void addHotel() {                                                                          //Add Method
+
+        Hotel LakeWood = new Hotel("LakeWood", 110, 80, 90, 80, 3);          //Adding Hotels Using Constructor in ArrayList
+        Hotel BridgeWood = new Hotel("BridgeWood", 150, 110, 50, 50, 4);     //HotelName - WeekDayRegularRate - WeekDayRewardRate - WeekendRegularRate - WeekendRewardRate - Rating
+        Hotel RidgeWood = new Hotel("RidgeWood", 220, 100, 150, 40, 5);
+
+        myHotelList.add(LakeWood);
+        myHotelList.add(BridgeWood);
+        myHotelList.add(RidgeWood);
+    }
 }
-
-
